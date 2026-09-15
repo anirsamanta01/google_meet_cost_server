ec30c2c73d5516b16f7ec85e2daabd33a41b0695
Anirban Samanta <anirbansamanata100@gmail.com>
2026-09-15 10:41:19 +0530

    change in server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import app from './app.js';
 import connectDB from './config/db.js';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 const startServer = async () => {
   try {
