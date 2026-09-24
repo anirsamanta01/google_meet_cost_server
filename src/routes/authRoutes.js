a0f0cb8e86436f7065aecd2048b3e33729ca199c
Anirban Samanta <anirbansamanata100@gmail.com>
2026-09-24 11:09:14 +0530

    signup

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import { signup, signin } from '../controllers/authController.js';
+import express from "express";
+import { signin, signup } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/signin', signin);
+router.post("/signin", signin);
+router.post("/signup", signup);
 
 export default router;
